315093cf2c76a3b469c617436923609e3464cc64
Daria Kuznetsova <ofgot17@gmail.com>
2025-01-04 23:14:06 +0100

    feat: add string to main

--- src/main/java/sir/smarthome/Main.java
+++ src/main/java/sir/smarthome/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose configuration which you want to see (1 or 2): ");
         Integer in = scanner.nextInt();
         SmartHomeApplication smartHomeApplication = new SmartHomeApplication();
         smartHomeApplication.run(in);
