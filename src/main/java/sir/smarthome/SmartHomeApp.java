82ece19ff217cdca0475792dded5f1925e921656
async-devil <async.devil@gmail.com>
2025-01-15 22:42:19 +0100

    chore: rename Main to SmartHomeApp

--- src/main/java/sir/smarthome/SmartHomeApp.java
+++ src/main/java/sir/smarthome/SmartHomeApp.java
@@ -2,7 +2,7 @@ package sir.smarthome;
 
 import java.util.Scanner;
 
-public class Main {
+public class SmartHomeApp {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Choose configuration which you want to see (1 or 2): ");
