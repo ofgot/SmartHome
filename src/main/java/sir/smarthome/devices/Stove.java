91bbba1556834871a4d87ec8ff60a7a6e8d2b89e
async-devil <async.devil@gmail.com>
2025-01-15 22:40:25 +0100

    refactor: refactor device and it's implements

--- src/main/java/sir/smarthome/devices/Stove.java
+++ src/main/java/sir/smarthome/devices/Stove.java
@@ -5,76 +5,12 @@ import sir.smarthome.common.Product;
 import java.util.Date;
 import java.util.UUID;
 
-public class Stove implements Device {
-    private UUID id;
-    private String name;
-    private double powerConsumption;
-    private double usageDuration;
-    private Date lastSync = new Date();
-    private boolean isTurnedOn = true;
-    private Date lastTurnOn = new Date();
-    private int condition;
-
-
+public class Stove extends Device {
     public Stove(String name, double powerConsumption) {
         this.id = UUID.randomUUID();
         this.name = name;
         this.powerConsumption = powerConsumption;
         this.usageDuration = 0;
         this.condition = 100;
-    }
-
-    @Override
-    public UUID getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public double getPowerConsumption() {
-        return powerConsumption;
-    }
-
-    @Override
-    public double getUsageDuration() {
-        return usageDuration;
-    }
-
-    @Override
-    public Date getLastTurnOn() {
-        return lastTurnOn;
-    }
-
-    @Override
-    public void turnOn() {
-        isTurnedOn = true;
-        lastTurnOn = new Date();
-    }
-
-    @Override
-    public void turnOff() {
-        isTurnedOn = false;
-    }
-
-    @Override
-    public void calculateUsageDuration() {
-        if (!isTurnedOn) return;
-        usageDuration += ((double) (new Date().getTime() - lastSync.getTime()) / 3600);
-        lastSync = new Date();
-    }
-
-    @Override
-    public double getUsageConsumption() {
-        calculateUsageDuration();
-        return usageDuration * powerConsumption;
-    }
-
-    @Override
-    public int getCondition() {
-        return this.condition;
     }
 }
--- src/main/java/sir/smarthome/devices/TV.java
+++ src/main/java/sir/smarthome/devices/TV.java
@@ -3,47 +3,13 @@ package sir.smarthome.devices;
 import java.io.DataOutput;
 import java.util.*;
 
-public class TV implements Device {
-    private UUID id;
-    private String name;
-    private double powerConsumption;
-    private double usageDuration;
-    private Date lastSync = new Date();
-    private boolean isTurnedOn = true;
-    private Date lastTurnOn = new Date();
-    private int condition;
-
+public class TV extends Device {
     public TV(String name, double powerConsumption) {
         this.id = UUID.randomUUID();
         this.name = name;
         this.powerConsumption = powerConsumption;
         this.usageDuration = 0;
         this.condition = 100;
-    }
-
-    @Override
-    public UUID getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public double getPowerConsumption() {
-        return powerConsumption;
-    }
-
-    @Override
-    public double getUsageDuration() {
-        return usageDuration;
-    }
-
-    @Override
-    public Date getLastTurnOn() {
-        return lastTurnOn;
     }
 
     public void changeChannel() {
@@ -56,34 +22,5 @@ public class TV implements Device {
 
     public void decreaseVolume(int decrement) {
         System.out.println("Volume decreased by " + decrement);
-    }
-
-    @Override
-    public void turnOn() {
-        isTurnedOn = true;
-        lastTurnOn = new Date();
-    }
-
-    @Override
-    public void turnOff() {
-        isTurnedOn = false;
-    }
-
-    @Override
-    public void calculateUsageDuration() {
-        if (!isTurnedOn) return;
-        usageDuration += ((double) (new Date().getTime() - lastSync.getTime()) / 3600);
-        lastSync = new Date();
-    }
-
-    @Override
-    public double getUsageConsumption() {
-        calculateUsageDuration();
-        return usageDuration * powerConsumption;
-    }
-
-    @Override
-    public int getCondition() {
-        return this.condition;
     }
 }
