1dfbf55ad8034b7ac2386577c3c0d32319b11944
async-devil <async.devil@gmail.com>
2025-01-15 22:41:05 +0100

    refactor: refactor magic number

--- src/main/java/sir/smarthome/common/Common.java
+++ src/main/java/sir/smarthome/common/Common.java
@@ -3,12 +3,13 @@ package sir.smarthome.common;
 import java.util.Random;
 
 public class Common {
-    public static final int MINTEMPERATURE = 15;
-    public static final int MAXTEMPERATURE = 30;
+    public static final int MIN_TEMPERATURE = 15;
+    public static final int MAX_TEMPERATURE = 30;
+    public static final int TEMPERATURE_BORDER_LINE = 22;
 
     public static int getTemperature() {
         Random random = new Random();
-        int temperature = random.nextInt(MAXTEMPERATURE - MINTEMPERATURE + 1) + MINTEMPERATURE;
+        int temperature = random.nextInt(MAX_TEMPERATURE - MIN_TEMPERATURE + 1) + MIN_TEMPERATURE;
         System.out.println("Temperature is " + temperature);
         return temperature;
     }
